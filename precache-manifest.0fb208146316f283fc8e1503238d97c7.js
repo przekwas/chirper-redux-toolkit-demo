79b9141e75546b688b14b3a0be0b8b58baf99d1d
przekwas <luke@covalence.io>
2020-03-04 00:59:28 -0600

    Updates

---
+++ precache-manifest.0fb208146316f283fc8e1503238d97c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bdaeb41f85b116590dd504c0a584be6",
+    "url": "/chirper-redux-toolkit-demo/index.html"
+  },
+  {
+    "revision": "8370a361f7be8d98091d",
+    "url": "/chirper-redux-toolkit-demo/static/js/2.94a93991.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/chirper-redux-toolkit-demo/static/js/2.94a93991.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "509b9fcdd0d1be6ed444",
+    "url": "/chirper-redux-toolkit-demo/static/js/main.6721975c.chunk.js"
+  },
+  {
+    "revision": "0c5394f1539e11bb3a30",
+    "url": "/chirper-redux-toolkit-demo/static/js/runtime-main.f7b1de2b.js"
+  }
+]);
